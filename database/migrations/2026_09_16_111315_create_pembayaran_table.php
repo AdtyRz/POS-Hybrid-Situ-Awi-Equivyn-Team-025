ce84e1877df7890da375f891d15c4df35fb6063f
AdtyRz <aditya.riqi1003@gmail.com>
2026-09-20 12:28:52 +0700

    feat(db): implement 3NF migrations & SituAwiDummy seeder

--- database/migrations/2026_09_16_111315_create_pembayaran_table.php
+++ database/migrations/2026_09_16_111315_create_pembayaran_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('pembayaran', function (Blueprint $table) {
             $table->id();
             $table->foreignId('pesanan_id')->constrained('pesanan')->onDelete('cascade');
-            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null'); // ID Kasir
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
             $table->enum('metode_pembayaran', ['tunai', 'qris', 'debit', 'kredit']);
             $table->string('snap_token')->nullable();
             $table->string('status_pembayaran')->default('pending');
